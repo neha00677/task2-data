7ece5686577b3ed99d2cfcd90685cac322f981fc
neha00677 <n.eeha013@gmail.com>
2025-10-30 23:40:44 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,8 +1,8 @@
 import os
 from groq import Groq
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def build_prompt(query, retrieved_faqs):
@@ -18,8 +18,8 @@ Customer Query: {query}
 Answer:"""
 
 def generate_answer(prompt):
-    chat_completion = client.chat.completions.create(
+    response = client.chat.completions.create(
         messages=[{"role": "user", "content": prompt}],
         model="llama-3.1-8b-instant",
     )
-    return chat_completion.choices[0].message.content
+    return response.choices[0].message.content
